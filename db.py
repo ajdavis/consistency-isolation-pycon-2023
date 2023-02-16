200d35d1bda5dc424fa0535d62b716674bed4f56
A. Jesse Jiryu Davis <jesse@emptysquare.net>
2023-02-16 17:38:02 -0500

    Initial commit

---
+++ db.py
@@ -0,0 +1,16 @@
+from flask import abort, Flask, request
+
+app = Flask(__name__)
+db = {}
+
+
+@app.route('/key/<key>', methods=['GET', 'PUT'])
+def key_handler(key: str):
+    if request.method == 'GET':
+        try:
+            return db[key]
+        except KeyError:
+            abort(404)
+    else:
+        db[key] = request.get_data()
+        return ''
