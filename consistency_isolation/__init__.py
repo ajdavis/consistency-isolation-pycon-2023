70499204ed54e3425d8c6f2a1b292b90162967c3
A. Jesse Jiryu Davis <jesse@emptysquare.net>
2023-04-14 23:10:43 -0400

    accept concurrent connections

--- consistency_isolation/__init__.py
+++ consistency_isolation/__init__.py
@@ -79,7 +79,7 @@ class Server:
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self._sock.bind(('', 23), )
-        self._sock.listen(1)
+        self._sock.listen(10)
 
         while True:
             sock, addr = self._sock.accept()
