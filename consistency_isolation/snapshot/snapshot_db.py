44adb9aa78bd7c46d61033256e0e60d72d7ae6d6
A. Jesse Jiryu Davis <jesse@emptysquare.net>
2023-04-16 17:21:15 -0400

    fix snapshot_db.py

--- consistency_isolation/snapshot/snapshot_db.py
+++ consistency_isolation/snapshot/snapshot_db.py
@@ -7,7 +7,7 @@ lock = threading.Lock()
 
 def server_thread(server: ServerConnection):
     with lock:
-        txn = db.copy()  # Create a snapshot.
+        snapshot = db.copy(); writes = {}
 
     while True:
         cmd = server.next_command()
@@ -15,16 +15,16 @@ def server_thread(server: ServerConnection):
             break
         elif cmd.name == "set":
             # Store writes in txn dict.
-            txn[cmd.key] = cmd.value
+            writes[cmd.key] = snapshot[cmd.key] = cmd.value
         elif cmd.name == "get":
             try:
-                server.send(txn[cmd.key])
+                server.send(snapshot[cmd.key])
             except KeyError:
                 server.send("not found")
         elif cmd.name == "commit":
             with lock:
-                db.update(txn)
-                txn = db.copy()
+                db.update(writes)
+                snapshot = db.copy(); writes = {}
 
 
 Server().serve(server_thread)
