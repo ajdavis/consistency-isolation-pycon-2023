6b3e89defb03e830784813892822954430b823cb
A. Jesse Jiryu Davis <jesse@emptysquare.net>
2023-04-21 08:51:17 -0600

    snapshot fixes

--- consistency_isolation/snapshot/snapshot_db.py
+++ consistency_isolation/snapshot/snapshot_db.py
@@ -1,21 +1,24 @@
+import collections
 import threading
 
 from .. import Server, ServerConnection
 
-db: dict[str, str] = {}
-lock = threading.Lock()
+db = {}
+write_time = collections.defaultdict(int)
+next_time = 1
+lock = threading.Lock()  # Guards db, write_time, and next_time.
 
 def server_thread(server: ServerConnection):
+    global next_time
     with lock:
-        snapshot = db.copy(); writes = {}
+        snapshot = db.copy(); writes = {}; start = next_time; next_time += 1
 
     while True:
         cmd = server.next_command()
         if cmd is None or cmd.name == "bye":
             break
         elif cmd.name == "set":
-            # Store writes in txn dict.
-            writes[cmd.key] = snapshot[cmd.key] = cmd.value
+            writes[cmd.key] = snapshot[cmd.key] = cmd.value  # Store writes in txn dict.
         elif cmd.name == "get":
             try:
                 server.send(snapshot[cmd.key])
@@ -23,8 +26,14 @@ def server_thread(server: ServerConnection):
                 server.send("not found")
         elif cmd.name == "commit":
             with lock:
-                db.update(writes)
-                snapshot = db.copy(); writes = {}
+                commit_time = next_time; next_time += 1
+                # Another txn wrote to the same key(s) and committed.
+                if any(start <= write_time[k] <= commit_time for k in writes):
+                    server.send("aborted")
+                else:
+                    write_time.update({k: commit_time for k in writes})
+                    db.update(writes)
+                snapshot = db.copy(); writes = {}; start = next_time; next_time += 1
 
 
 Server().serve(server_thread)
